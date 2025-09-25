aee0c24f353fc1276bf43efbc128cd963ea6ebff
Hari-chaitanya-1 <harichaitanya47@gmail.com>
2025-09-25 13:58:55 +0530

    changed database connection

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -17,7 +17,7 @@ const ContactForm = () => {
     setStatus("Submitting...");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contacts", {
+      const response = await fetch("https://portfolio-full-stack-0n9m.onrender.com/api/contacts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
