2de1774701c95bae8ddc44c5c547c2af8c9a19d6
Hari-chaitanya-1 <harichaitanya47@gmail.com>
2025-09-25 13:43:27 +0530

    changed to deploy backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.log('MongoDB connection failed:', error.message);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI); 
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+  }
+};
 
-module.exports = connectDB;
+export default connectDB;
